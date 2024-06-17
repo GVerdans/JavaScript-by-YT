562b77ad5f34b5cbf7024cb08ed69aae8dd6a365
GVerdans <149999884+GVerdans@users.noreply.github.com>
2024-06-17 17:16:05 -0300

    add aula 15 ( Arrays )

---
+++ Aula 15/ambiente.js
@@ -0,0 +1,11 @@
+// Arrays
+
+let num = [5,8,2,9,3];
+
+num.push(15); // Empurra na ultima posição.
+
+// console.log(num.length); num.length tamanho do array.
+// num.sort(); Organiza do Maior pro Menor.
+
+console.log(num.length);
+console.log(num.indexOf(9));
