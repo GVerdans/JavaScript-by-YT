638f098f7275639a836afbb7c7bfc518057b1afd
GVerdans <149999884+GVerdans@users.noreply.github.com>
2024-07-01 12:02:04 -0300

    att aula 17ex. quse concluido ex17

---
+++ Aula 17/Aula 17ex/Lista/script copy.js
@@ -0,0 +1,8 @@
+let yarra = [10, 20, 30, 40];
+let soma = 0;
+
+for (let i = 0 ; i < yarra.length ; i++){
+    soma+= yarra[i];
+}
+
+console.log(soma);
--- Aula 17/Aula 17ex/Lista/script.js
+++ Aula 17/Aula 17ex/Lista/script.js
@@ -1,25 +1,40 @@
 let numeros = document.querySelector('input#input_num');
 let select_saida = document.querySelector('select#select_result');
-let yarra = [];
 let resultado = document.querySelector('div#res');
+let yarra = [];
+
+function isNumero(n) {
+	if (Number(n) >= 1 && Number(n) <= 100) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function inLista(n, l) {
+	if (l.indexOf(Number(n)) != -1) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 
-function adicionar(){
 
-    yarra.push(numeros.value);
+function adicionar() {
+	if (isNumero(numeros.value) && !inLista(numeros.value, yarra)) {
+		yarra.push(Number(numeros.value));
 
-if (numeros.value < 1 || numeros.value > 100){
-        alert('Valor inválido ou já se encontra na lista !');
+		let item = document.createElement('option');
+		item.text = `Valor ${numeros.value} foi adicionado`;
+		select_saida.appendChild(item);
+		resultado.innerHTML = ''
 
-    } else {
-
-        const valor_select = document.createElement('option');
-
-        valor_select.innerHTML = `O valor ${numeros.value} foi adicionado.`;
-
-        select_saida.appendChild(valor_select);
-    }
-    
+	} else {
+		alert('Valor inválido ou já se encontra na lista !');
+	}
+	numeros.value = ''
+	numeros.focus()
 }
 
 
@@ -28,9 +43,21 @@ if (numeros.value < 1 || numeros.value > 100){
 
 function finalizar() {
 
-resultado.innerHTML += `<p> Ao todo temos ${yarra.length} números cadastrados. </p>`
-resultado.innerHTML += `<p> O maior valor informado foi ${Math.max.apply(null, yarra)}. </p>`
+	if (yarra.length == 0) {
+		alert('Adicione valores antes de finalizar !')
+	} else {
+
+		let somaYarra = 0;
+		for (let i = 0; i < yarra.length; i++) {
+			somaYarra += yarra[i];
+		}
 
 
+		resultado.innerHTML += `<p> Ao todo temos ${yarra.length} números cadastrados. </p>`
+		resultado.innerHTML += `<p> O maior valor informado foi ${Math.max.apply(null, yarra)}. </p>`
+		resultado.innerHTML += `<p> O menor valor informado foi ${Math.min.apply(null, yarra)}. </p>`
 
+
+		resultado.innerHTML += `<p> O soma dos valores é engual ${somaYarra}. </p>`
+	}
 }
